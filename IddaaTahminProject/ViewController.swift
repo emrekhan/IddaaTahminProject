257e8bf4c33a9e937265ccebe4a5d7637c005d2a
burakyldz <burak_yldz@hotmail.com.tr>
2016-01-06 14:29:07 +0200

    Deneme
    Deneme

--- IddaaTahminProject/ViewController.swift
+++ IddaaTahminProject/ViewController.swift
@@ -9,17 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var pageMenu : CAPSPageMenu?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "PAGE MENU"
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        
+//        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "<-", style: UIBarButtonItemStyle.Done, target: self, action: "didTapGoToLeft")
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "->", style: UIBarButtonItemStyle.Done, target: self, action: "didTapGoToRight")
+        
+        
+        var controllerArray : [UIViewController] = []
+        
+        
+        let test1:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("test1ViewController") as UIViewController
+        //self.presentViewController(controller5, animated: false, completion: nil)
+        test1.title = "Test1"
+        controllerArray.append(test1)
+        
+        let test2:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("test2ViewController") as UIViewController
+        //self.presentViewController(controller5, animated: false, completion: nil)
+        test2.title = "Test2"
+        controllerArray.append(test2)
+        
+        let test3:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("test3ViewController") as UIViewController
+        //self.presentViewController(controller5, animated: false, completion: nil)
+        test3.title = "Test3"
+        controllerArray.append(test3)
+        
+        // Customize menu (Optional)
+        let parameters: [CAPSPageMenuOption] = [
+            .ScrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
+            .ViewBackgroundColor(UIColor.greenColor()),
+            .SelectionIndicatorColor(UIColor.whiteColor()),
+            .BottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
+            .MenuItemFont(UIFont(name: "HelveticaNeue", size: 18.0)!),
+            .MenuHeight(40.0),
+            .MenuItemWidth(90.0),
+            .CenterMenuItems(true)
+        ]
+        
+        // Initialize scroll menu
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+        
+        self.addChildViewController(pageMenu!)
+        self.view.addSubview(pageMenu!.view)
+        
+        pageMenu!.didMoveToParentViewController(self)
+        
+        
+        
     }
-
+    
+    func didTapGoToLeft() {
+        let currentIndex = pageMenu!.currentPageIndex
+        
+        if currentIndex > 0 {
+            pageMenu!.moveToPage(currentIndex - 1)
+        }
+    }
+    
+    func didTapGoToRight() {
+        let currentIndex = pageMenu!.currentPageIndex
+        
+        if currentIndex < pageMenu!.controllerArray.count {
+            pageMenu!.moveToPage(currentIndex + 1)
+        }
+    }
+    
+    // MARK: - Container View Controller
+    override func shouldAutomaticallyForwardAppearanceMethods() -> Bool {
+        return true
+    }
+    
+    override func shouldAutomaticallyForwardRotationMethods() -> Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    
 }
 
---
+++ IddaaTahminProject/test3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  test3ViewController.swift
+//  IddaaTahminProject
+//
+//  Created by BURAK YILDIZ on 31.12.2015.
+//  Copyright © 2015 BURAK YILDIZ. All rights reserved.
+//
+
+import UIKit
+
+class test3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         self.view.backgroundColor = UIColor.greenColor()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
