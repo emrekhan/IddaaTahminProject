ee88b00a0f2bd09544eb8fa13a997f57b56869f4
emrekhan <emre_esen@hotmail.com.tr>
2016-01-08 01:47:48 +0200

    son2
    emotkhan

--- IddaaTahminProject/AppDelegate.swift
+++ IddaaTahminProject/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         print("huhu aqeee")
+        print("emotkhan")
         
         return true
     }
